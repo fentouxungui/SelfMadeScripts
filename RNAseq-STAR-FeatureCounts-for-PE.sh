2ef4bca45df3230f39f9260c905c0d433c0c91f7
fentouxungui <43262531+fentouxungui@users.noreply.github.com>
2019-12-30 10:57:46 +0800

    fixed some errors
    修改了参数传递错误

---
+++ RNAseq-STAR-FeatureCounts-for-PE.sh
@@ -0,0 +1,338 @@
+#!/bin/bash
+# Using getopt
+
+abort()
+{
+    echo >&2 '
+***************
+*** ABORTED ***
+***************
+'
+    echo "An error occurred. Exiting..." >&2
+    exit 1
+}
+
+trap 'abort' 0
+
+set -e
+
+################################################################
+## It assumes fastqc,trim_galore,STAR,bedtools,samtools,RSeQC,
+## featurecounts and multiQC are all available.
+## We have tested this script on an Centos system.
+################################################################
+
+threads=24
+length=25
+output=results
+
+# Augument Parsing
+print_usage_and_exit(){
+    echo "Usage: $0 [-f <path of fastq files> ] [-o <output dir name default: results>] [-j <number of threads> = 24 default] [-m <Galore minimum length> =$length] -i <star index> =path/to/STAR/index -b <genes bed file> =path/to/genes.bed -g <genes gtf file> =path/to/genes.gtf "
+    exit 1
+}
+
+while getopts ":f:j:o:m:i:b:g:h:" opt; do
+    case $opt in
+        f)
+            fastq="$OPTARG"
+            echo "-d <fastq dir> = $fastq"
+            ;;
+        o)
+            output="$OPTARG"
+            echo "-o <output dir> = $output"
+            ;;
+        j)
+            threads="$OPTARG"
+            echo "-j <threads used> = $threads"
+            ;;
+        m)
+            length="$OPTARG"
+            echo "-m <Galore minimum length> = $length"
+            ;;
+        i)
+            star_index="$OPTARG"
+            echo "-i <STAR index> = $star_index"
+            ;;
+        g)
+            genes_gtf="$OPTARG"
+            echo "-g <genes gtf file> = $genes_gtf"
+            ;;
+        b)
+            genes_bed="$OPTARG"
+            echo "-b <genes bed file> = $genes_bed"
+            ;;
+        h)
+            print_usage_and_exit
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG"
+            print_usage_and_exit
+            ;;
+        :)
+            echo "Option -$OPTARG requires an argument."
+            print_usage_and_exit
+            ;;
+    esac
+done
+
+# Required arguments
+if [ -z "$star_index" -o -z "$genes_gtf" -o -z "$genes_bed" ]
+then
+    echo "Error: missing required argument(s)"
+    print_usage_and_exit
+fi
+
+if [ -d $fastq ]
+then
+    echo "fastq dir found! "
+else
+    echo "fastq dir not exist! Please input correct fastq dir! "
+    print_usage_and_exit
+fi
+
+if [ -f $genes_bed ]
+then
+    echo "Genes bed file found! "
+else
+    echo "Genes bed file not exist! Please input correct Genes bed file! "
+    print_usage_and_exit
+fi
+
+if [ -f $genes_gtf ]
+then
+    echo "Genes GTF file found! "
+else
+    echo "Genes GTF file not exist! Please input correct Genes GTF file! "
+    print_usage_and_exit
+fi
+
+if [ -d ${star_index%\/*} ]
+then
+    echo "STAR index dir found! "
+else
+    echo "STAR index dir not exist! Please input correct STAR index dir! "
+    print_usage_and_exit
+fi
+
+
+
+
+echo "#******************************************************************#"
+echo "RNA-Seq Pipline (STAR + FeatureCounts) for PE data Starts At:"
+starttime=`date +'%Y-%m-%d %H:%M:%S'`
+echo "程序开始于： "${starttime}""
+echo ""
+echo "         Step 1. Analysing Sequence Quality with FastQC             "
+echo "#******************************************************************#"
+mkdir -p ${output}
+cd ${output}
+mkdir -p 1_initial_qc
+cd 1_initial_qc
+abpath=$(pwd)
+cd ../../${fastq}
+
+fastqc *.gz \
+-t $threads \
+-o $abpath \
+--noextract
+wait
+
+echo "#******************************************************************#"
+echo "#     Step 2:  Removing Low Quality Sequences with Trim_Galore     #"
+echo "#******************************************************************#"
+cd ../${output}
+mkdir -p 2_trimmed_output
+cd 2_trimmed_output
+abpath=$(pwd)
+cd ../../${fastq}
+
+ls *.fastq.gz | grep 'R1'| while read id;
+do
+trim_galore \
+--cores $threads \
+--fastqc \
+--length $length \
+--output_dir $abpath \
+--paired $id ${id/R1/R2}
+done
+
+
+echo "#******************************************************************#"
+echo "#          Step 3: Aligning to Genome with STAR-aligner            #"
+echo "#******************************************************************#"
+cd ../${output}
+mkdir -p 3_aligned_STAR
+cd 3_aligned_STAR
+star_path=$(pwd)
+cd ../2_trimmed_output
+
+ls *val_1.fq.gz | grep "_R1_" | while read id;
+do
+tmp=${id/R1/R2}
+STAR \
+--readFilesCommand zcat \
+--genomeDir $star_index \
+--readFilesIn $id ${tmp/val_1/val_2} \
+--runThreadN 16 \
+--outSAMtype BAM SortedByCoordinate \
+--quantMode GeneCounts \
+--outFileNamePrefix $star_path/${id%trimmed.fq.gz}
+done
+wait
+
+cd ../3_aligned_STAR
+
+ls *.bam | while read id; do 
+samtools index ${id} 
+done
+wait
+
+
+echo "#******************************************************************#"
+echo "#           Step 4:  Generate BW files with bamCoverage            "
+echo "#******************************************************************#"
+cd ../
+mkdir -p 4_bw_files
+cd 3_aligned_STAR
+
+ls *.bam | while read id;
+do
+bamCoverage -b ${id} \
+-o ../4_bw_files/${id%.bam}.RPKM.bw \
+-of bigwig \
+-p $threads \
+--normalizeUsing RPKM
+done
+wait
+
+num_bam=`ls -l *.bam | wc -l`
+
+if [ $num_bam -ge 3 ]
+then
+    multiBamSummary bins \
+    --bamfiles *.bam \
+    --minMappingQuality 30 \
+    -out readCounts.npz --outRawCounts readCounts.tab
+
+    plotCorrelation \
+        -in readCounts.npz \
+        --corMethod pearson --skipZeros \
+        --plotTitle "Pearson Correlation of Read Counts" \
+        --whatToPlot scatterplot \
+        -o scatterplot_PearsonCorr_BAMScores.png   \
+        --outFileCorMatrix PearsonCorr_BAMScores.tab
+
+    plotCorrelation \
+        -in readCounts.npz \
+        --corMethod spearman --skipZeros \
+        --plotTitle "Spearman Correlation of Read Counts" \
+        --whatToPlot scatterplot \
+        -o scatterplot_SpearmanCorr_BAMScores.png   \
+        --outFileCorMatrix SpearmanCorr_BAMScores.tab
+
+    plotCorrelation \
+        -in readCounts.npz \
+        --corMethod spearman --skipZeros \
+        --plotTitle "Spearman Correlation of Read Counts" \
+        --whatToPlot heatmap --colorMap RdYlBu --plotNumbers \
+        -o heatmap_SpearmanCorr_readCounts.png   \
+        --outFileCorMatrix SpearmanCorr_readCounts.tab
+
+    plotCorrelation \
+        -in readCounts.npz \
+        --corMethod pearson --skipZeros \
+        --plotTitle "Pearson Correlation of Read Counts" \
+        --whatToPlot heatmap --colorMap RdYlBu --plotNumbers \
+        -o heatmap_PearsonCorr_readCounts.png   \
+        --outFileCorMatrix PearsonCorr_readCounts.tab
+fi &
+
+echo "#******************************************************************#"
+echo "#                  Step 5: RNAseq QC with RSeQC                    #"
+echo "#******************************************************************#"
+cd ../
+mkdir -p 5_RSeQC_report
+cd 3_aligned_STAR
+
+ls *.bam | while read id;
+do
+bam_stat.py \
+-i ${id} \
+> ../5_RSeQC_report/${id%.bam}.RSeQC.bam_stat.txt &
+done
+
+ls *.bam | while read id;
+do
+read_distribution.py \
+-i ${id} -r $genes_bed \
+> ../5_RSeQC_report/${id%.bam}.RSeQC.reads_distribution.txt &
+done
+
+task=bam_stat.py
+while ps -ef | grep $task | grep -v 'grep'; do
+sleep 10
+done
+echo "bam_stat.py finished! "
+
+
+task=read_distributi
+while ps -ef | grep $task | grep -v 'grep'; do
+sleep 10
+done
+echo "read_distributiion.py finished! "
+
+task=multiBamSummary
+while ps -ef | grep $task | grep -v 'grep'; do
+echo "multiBamSummary is running!"
+sleep 30
+done
+echo "multiBamSummary finished! "
+
+task=plotCorrelation
+while ps -ef | grep $task | grep -v 'grep'; do
+sleep 15
+done
+echo "plotCorrelation finished! "
+
+echo "#******************************************************************#"
+echo "#        Step 6: Summarizing Gene Counts with featureCounts        #"
+echo "#******************************************************************#"
+cd ../
+mkdir -p 6_final_counts
+cd 3_aligned_STAR
+
+dirlist=$(ls -t ./*.bam | tr '\n' ' ')
+
+featureCounts \
+-a $genes_gtf \
+-F GTF \
+-o ../6_final_counts/final_counts.txt \
+-g 'gene_name' \
+-T 6 \
+-M \
+$dirlist
+
+
+echo "#******************************************************************#"
+echo "#        Step 7: Generating analysis report with multiQC           #"
+echo "#******************************************************************#"
+cd ../
+mkdir -p 7_multiQC
+cd ../
+
+multiqc ${output} \
+--outdir ${output}/7_multiQC
+
+
+echo "#******************************************************************#"
+echo "Congratulations! Whole processs finished! "
+echo "RNA-Seq Pipline Finished At:"
+endtime=`date +'%Y-%m-%d %H:%M:%S'`
+echo "${endtime}"
+start_seconds=$(date --date="$starttime" +%s);
+end_seconds=$(date --date="$endtime" +%s);
+secondsdiff=$((end_seconds-start_seconds))
+echo "程序总运行时间： "$((secondsdiff/3600))" h "$((secondsdiff%3600/60))" m"
+trap : 0
+echo >&2 '#******************************************************************#'
---
+++ ShinyAapp-SeuratSimple.r
@@ -0,0 +1,214 @@
+library(Seurat)
+library(stringr)
+library(shiny)
+library(ggplot2)
+library(jpeg)
+library(dplyr)
+library(shinydashboard)
+library(DT)
+#********************************************************************************************************************#
+# @Varibles
+# importdata: ClusterMarkers,FileName
+# ui: FeaturePlot, DimensionReduction, GeneSymbol, UpdatePlot, DownloadPlot, Group1, Group2, ClusterInfo,CalculateDEG
+# server: PlotFeature, Reaction, DownloadDEG, Markers, MarkersTable
+
+# @Attention
+# 1. no ".","-" in variable names! and alse the dashboard name!
+# 2. RunApp with external website will give a correct filename!
+#********************************************************************************************************************#
+
+# data settings
+EC <- readRDS("EC-dims15-res0.5.rds")
+EC@project.name <- "EC"
+ECMarkers <- read.csv("markers-EC-dims15.csv")
+
+APC <- readRDS("scRNA_APC.rds")
+APC@project.name <- "APC"
+APCMarkers <- read.csv("markers_APC_cluster.csv")
+
+Ttk5d <- readRDS("Ttk-5d-dims15-res0.5.rds")
+Ttk5d@project.name <- "Ttk-5d"
+Ttk5dMarkers <- read.csv("markers-Ttk-5d-cluster.csv")
+
+Ttk14drep1 <- readRDS("Ttk-14d-rep1-dims11-res0.5.rds")
+Ttk14drep1@project.name <- "Ttk-14d-rep1"
+Ttk14drep1dMarkers <- read.csv("markers-Ttk-14d-rep1-cluster.csv")
+
+Ttk14drep2 <- readRDS("Ttk-14d-rep2-dims15-res0.5.rds")
+Ttk14drep2@project.name <- "Ttk-14d-rep2"
+Ttk14drep2Markers <- read.csv("markers-Ttk-14d-rep2-cluster.csv")
+
+## app.R ##
+ui <- dashboardPage(
+  dashboardHeader(title = "Lab of Xi Rongwen"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("About This Site", tabName = "widgets", icon = icon("th")),
+      menuItem("Fly gut scRNAseq", tabName = "dashboard", icon = icon("dashboard"))
+    )
+  ),
+  dashboardBody(
+    tabItems(
+      # First tab content
+      tabItem(tabName = "dashboard",
+              fluidRow(
+                box(title = "Scatter Plot",plotOutput("FeaturePlot",height = "550px"),status = "primary",collapsible = TRUE,solidHeader = TRUE),
+                box(title = "Settings",solidHeader = TRUE,
+                    selectInput("dataset", "Choose a dataset:", 
+                                choices = c(
+                                  "EC", 
+                                  "APC","Ttk-5d",
+                                  "Ttk-14d-rep1",
+                                  "Ttk-14d-rep2"
+                                  )),
+                    helpText(""),
+                    selectInput("DimensionReduction","Dimension Reduction:",choices = c("UMAP" = "umap","tSNE" = "tsne")),
+                    textInput("GeneSymbol","Gene Symbol:",value = ""),
+                    helpText("Note: please input a gene symbol(not case-sensitive,except for genes like Dl,dl,H,hald,Ald,and so on), 
+                             if no gene symbols, using CG number instead. Also support for nFeature_RNA, percent.mt, nCount_RNA."),
+                    helpText(""),
+                    helpText(""),
+                    actionButton("UpdatePlot", "Update View"),
+                    helpText(""),
+                    downloadButton('DownloadPlot', label = 'Download Plot'),
+                    helpText(""),
+                    textInput("Group1","Group 1:",value = "1"),
+                    textInput("Group2","Group 2:",value = "2"),
+                    actionButton("CalculateDEG", "Calculate DEGs!"),
+                    helpText(""),
+                    status = "primary",collapsible = TRUE)
+                ),
+              fluidRow(
+                box(height = "850px",solidHeader = TRUE,
+                    title = "Cluster Info",
+                    plotOutput("ClusterInfo"),collapsible = TRUE,status = "primary"
+                ),
+                box(solidHeader = TRUE,
+                  title = "Top 10 Markers of each Cluster By log(FC)",
+                  DT::dataTableOutput("MarkersTable"),collapsible = TRUE,status = "primary"
+                )
+              )   
+      ) ,
+      # About this site
+      tabItem(tabName = "widgets",
+              h1("Welcome to Xi Lab!",align = "center"),
+              h2(""),
+              h5("A Database Only for Internal Use.",align = "center"),
+              h2("")
+      )   
+    )
+  )
+)
+
+
+
+server <- function(input, output,session) {
+  # Return the requested dataset
+  datasetInput <- reactive({
+    switch(input$dataset,
+           "EC" = EC,
+           "APC" = APC,
+           "Ttk-5d" = Ttk5d,
+           "Ttk-14d-rep1" = Ttk14drep1,
+           "Ttk-14d-rep2" = Ttk14drep2
+           )
+  })
+  # return the markers of each cluster
+  ClusterMarkers <- reactive({
+    if ( datasetInput()@project.name == "EC" ) { return(ECMarkers) }
+    if ( datasetInput()@project.name == "APC" ) { return(APCMarkers) }
+    if ( datasetInput()@project.name == "Ttk-5d" ) { return(Ttk5dMarkers) }
+    if ( datasetInput()@project.name == "Ttk-14d-rep1" ) { return(Ttk14drep1dMarkers) }
+    if ( datasetInput()@project.name == "Ttk-14d-rep2" ) { return(Ttk14drep2Markers) }
+  })
+  
+  # return cluster info plot
+  output$ClusterInfo <- renderPlot({
+    if ( datasetInput()@project.name == "EC" ) { clusterinfo <- readJPEG("EC-anno-small.jpg") }
+    if ( datasetInput()@project.name == "APC" ) { clusterinfo <- readJPEG("APC-anno-small.jpg") }
+    if ( datasetInput()@project.name == "Ttk-5d" ) { clusterinfo <- readJPEG("Ttk-5d-anno.jpg") }
+    if ( datasetInput()@project.name == "Ttk-14d-rep1" ) { clusterinfo <- readJPEG("Ttk-14d-rep1-anno.jpg") }
+    if ( datasetInput()@project.name == "Ttk-14d-rep2" ) { clusterinfo <- readJPEG("Ttk-14d-rep2-anno.jpg") }
+    
+    if(exists("rasterImage")){
+      plot(1:2, type='n',yaxt="n",xaxt="n",xlab="",ylab="")
+      rasterImage(clusterinfo,1,1,2,2)}
+  },
+  height = function() {
+    session$clientData$output_ClusterInfo_width}
+  )
+  
+  # Scatter Plot
+  PlotFeature <- reactive({
+    if (input$GeneSymbol == "") {
+      print(paste("scRNAseq-",datasetInput()@project.name,"-DimPlot-",input$DimensionReduction,sep=""))
+      DimPlot(datasetInput(),reduction = input$DimensionReduction,label = TRUE,coord.fixed = TRUE)
+    }else {
+      gene <- tolower(input$GeneSymbol)
+      gene_library <- tolower(rownames(datasetInput()@assays$RNA@data))
+      if (input$GeneSymbol %in% c(rownames(datasetInput()@assays$RNA@data),"nFeature_RNA", "percent.mt", "nCount_RNA")) {
+        print(paste("scRNAseq-",datasetInput()@project.name,"-FeaturePlot-",input$GeneSymbol,sep=""))
+        FeaturePlot(datasetInput(),features = input$GeneSymbol,cols = c("gray", "red"),
+                    label = TRUE,label.size = 7,coord.fixed = TRUE,reduction = input$DimensionReduction)
+      }else if(gene %in% gene_library) {
+        gene_name <- rownames(datasetInput()@assays$RNA@data)[gene_library == gene]
+        print(paste("scRNAseq-",datasetInput()@project.name,"-FeaturePlot-",gene_name,sep=""))
+        FeaturePlot(datasetInput(),features = gene_name,cols = c("gray", "red"),
+                    label = TRUE,label.size = 7,coord.fixed = TRUE,reduction = input$DimensionReduction)
+      }else{
+        error <- readJPEG("www/error.jpg")
+        if(exists("rasterImage")){
+          plot(1:2, type='n',yaxt="n",xaxt="n",xlab="",ylab="")
+          rasterImage(error,1,1,2,2)
+        }
+      }
+    }
+  })
+  
+  Reaction <- eventReactive(input$UpdatePlot,PlotFeature())
+  
+  output$FeaturePlot <- renderPlot({
+    if(input$UpdatePlot == 0){return(PlotFeature())}else{return(Reaction())}
+  })
+  
+  output$DownloadPlot <- downloadHandler(
+    print( paste("scRNAseq-",datasetInput()@project.name, "_",input$GeneSymbol, "_",input$DimensionReduction,'.png', sep='') ),
+    filename = function() { paste("scRNAseq-",datasetInput()@project.name, "_",input$GeneSymbol, "_",input$DimensionReduction,'.png', sep='') },
+    content = function(filename) {ggsave(filename,plot = PlotFeature(), 
+                                         # plot size are same with what client see in website
+                                         width = 10, height= 10,units="in",device = "png")
+    })
+  
+  Markers <- eventReactive(input$CalculateDEG, {
+    if (input$Group1 == "" & input$Group2 == "") {
+      print(paste("Download markers of each cluser from data - ",datasetInput()@project.name,sep = ""))
+      return(ClusterMarkers())}
+    else{
+      g1 <- unlist(strsplit(as.character(input$Group1),","))
+      g2 <- unlist(strsplit(as.character(input$Group2),","))
+      print(paste("Calculate DEGs between cluster",g1,"-and-",g2," using data-",datasetInput()@project.name,sep=""))
+      FindMarkers(datasetInput(), ident.1 = as.numeric(g1), ident.2 = as.numeric(g2), min.pct = 0.25)
+    }
+  })
+  
+  observeEvent(input$CalculateDEG,{
+    if(!is.na(input$CalculateDEG)) {
+      removeUI("#DownloadDEG")}
+  })
+  
+  observeEvent(input$CalculateDEG,{
+    insertUI(selector = "#CalculateDEG", where = "afterEnd", ui = downloadButton("DownloadDEG",label = "Download DEGs!"))
+  })
+
+  output$DownloadDEG <- downloadHandler(
+    filename = function(){paste("scRNAseq-",datasetInput()@project.name, "_cluster",gsub(",","-",input$Group1),"_Vs_",gsub(",","-",input$Group2),".csv", sep = "")},
+    content = function(filename) {write.csv(Markers(), filename)}
+  )
+  
+  output$MarkersTable <- DT::renderDataTable(
+    ClusterMarkers() %>%  group_by(cluster) %>% top_n(n = 10, wt = avg_logFC) %>% DT::datatable(options = list(scrollX = TRUE)) %>% 
+      DT::formatRound(c('avg_logFC')) %>% DT::formatSignif(c('p_val','p_val_adj'))
+  )
+}
+
+shinyApp(ui, server)
